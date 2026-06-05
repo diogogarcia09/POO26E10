55d56c829c293aca3b02269d566559b95887a26f
Diogo <Diogo@10.179.9.148>
2026-06-05 15:15:54 +0100

    classe utilizador atualizada

--- POO26E10/src/poo26e10/Utilizador.java
+++ POO26E10/src/poo26e10/Utilizador.java
@@ -6,18 +6,39 @@ public class Utilizador {
 	private String email;
 	private String password;
 	private String tipo;
+	private int tentativasLogin;
+	private boolean bloqueado;
+	
+	
 	public Utilizador(String nome, String numId, String email, String password, String tipo) {
-		super();
 		this.nome = nome;
 		this.numId = numId;
 		this.email = email;
 		this.password = password;
 		this.tipo = tipo;
+		this.tentativasLogin = 0;
+		this.bloqueado = false;
+		
 	}
 	
 	
 	public boolean login(String numId, String password) {
-		return this.numId.equals(numId) && this.password.equals(password);
+		if (bloqueado) {
+			System.out.println("Conta bloquada. Tente mais tarde");
+			return false;
+		}
+		if (this.numId.equals(numId) && this.password.equals(password)) {
+			tentativasLogin = 0;
+			return true;
+		}
+		else {
+			tentativasLogin++;
+			if (tentativasLogin >= 3) {
+				bloqueado = true;
+				System.out.println("Conta bloqueada após 3 tentativas falhadas.");
+			}
+			return false;
+		}
 	}
 
 
@@ -43,6 +64,43 @@ public class Utilizador {
 
 	public String getTipo() {
 		return tipo;
+	}
+
+
+	public int getTentativasLogin() {
+		return tentativasLogin;
+	}
+
+
+	public boolean isBloqueado() {
+		return bloqueado;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public void setBloqueado(boolean bloqueado) {
+		this.bloqueado = bloqueado;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Utilizador [nome=" + nome + ", numId=" + numId + ", email=" + email + ", password=" + password
+				+ ", tipo=" + tipo + "]";
 	}
 	
 	

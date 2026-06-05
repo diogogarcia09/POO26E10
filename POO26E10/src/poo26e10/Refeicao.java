1638ae6c08f73a14c5ec5fc30f7e9a8a378b24f8
ines <ines@10.179.16.227>
2026-06-05 16:10:01 +0100

    classe Refeicao

---
+++ POO26E10/src/poo26e10/Refeicao.java
@@ -0,0 +1,63 @@
+package poo26e10;
+
+public class Refeicao {
+	
+	 private String idRefeicao;
+	    private double preco;
+	    private int quantDisponivel;
+	    private Produto produto;
+	    
+	    
+		public Refeicao(String idRefeicao, double preco, int quantDisponivel, Produto produto) {
+			this.idRefeicao = idRefeicao;
+			this.preco = preco;
+			this.quantDisponivel = quantDisponivel;
+			this.produto = produto;
+		}
+	    
+	    
+		public boolean esgotada() {
+	        return quantDisponivel == 0;
+		}
+
+
+		public double getPreco() {
+			return preco;
+		}
+
+
+		public void setPreco(double preco) {
+			this.preco = preco;
+		}
+
+
+		public int getQuantDisponivel() {
+			return quantDisponivel;
+		}
+
+
+		public void setQuantDisponivel(int quantDisponivel) {
+			this.quantDisponivel = quantDisponivel;
+		}
+
+
+		public String getIdRefeicao() {
+			return idRefeicao;
+		}
+
+
+		public Produto getProduto() {
+			return produto;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Refeicao [idRefeicao=" + idRefeicao + ", preco=" + preco + ", quantDisponivel=" + quantDisponivel
+					+ ", produto=" + produto + "]";
+		}
+		
+		
+}
+
+		

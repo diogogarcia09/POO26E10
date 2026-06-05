882bfaf386e3c2109702a85f24a02938451faebc
Diogo <Diogo@10.179.9.148>
2026-06-05 18:05:10 +0100

    Classe Estudante

--- POO26E10/src/poo26e10/Utilizador.java
+++ POO26E10/src/poo26e10/Utilizador.java
@@ -99,8 +99,7 @@ public class Utilizador {
 
 	@Override
 	public String toString() {
-		return "Utilizador [nome=" + nome + ", numId=" + numId + ", email=" + email + ", password=" + password
-				+ ", tipo=" + tipo + "]";
+	    return "Nome: " + nome + "\nID: " + numId + "\nEmail: " + email + "\nTipo: " + tipo;
 	}
 	
 	
